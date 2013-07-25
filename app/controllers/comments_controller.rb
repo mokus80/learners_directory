6f1cfc4626ecd949fc802904d8a8787d69fec291
Nicole <mokus80@gmail.com>
2013-07-25 16:11:18 +0200

    make users post comments, display number of comments on resources index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
+
   # GET /comments
   # GET /comments.json
   def index
@@ -23,19 +24,26 @@ class CommentsController < ApplicationController
 
   # POST /comments
   # POST /comments.json
+
   def create
     @comment = Comment.new(comment_params)
-
+    @comment.resource_id = params[:resource_id]
+    @comment.user_id = current_user.id
+    @comment.save
     respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @comment }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to resource_path(@resource), :notice => "Your comment has been saved" }
+      format.js
     end
   end
+  # if @comment.save
+  #       format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+  #       format.json { render action: 'show', status: :created, location: @comment }
+  #     else
+  #       format.html { render action: 'new' }
+  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,20 +1,5 @@
 class RatingsController < ApplicationController
-	# def create
-	# 	@resource = Resource.find_by(params[:resource_id])
-	# 	if current_user.id == @resource.id
-	# 		redirect_to resource_path(@resource), :alert => "You cannot rate your own resource"
-	# 	else
-	# 		@rating = Rating.new ratings_params
-	# 		@rating.resource_id = @resource.id
-	# 		@rating.user_id = current_user.id
-	# 		if @rating.save
-	# 			respond_to do |format|
-	# 				format.html { redirect_to resource_path(@resource), :notice => "Your rating has been saved" }
-	# 				format.js
-	# 			end
-	# 		end
-	# 	end
-	# end
+	
 
 	def create
 		@rating = Rating.new ratings_params
