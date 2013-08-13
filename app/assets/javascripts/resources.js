4adc424b5e825fca205f48dda7031c663317f011
Nicole <mokus80@gmail.com>
2013-08-13 15:20:59 +0200

    make popup window for 'delete' actions, doesnt always work yet

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
 	$('.rating-button').click(function() {
 		$(this).closest('.resource-box').find('.rating-box').css("visibility", "visible");
 	});
+	$('a.action.delete-button').click(function(){
+		return confirm('Are you sure you want to delete this resource?')
+	});
 }); 
+
 
 //'$(function(){}) is same as '$(document).ready(function() {}) 
