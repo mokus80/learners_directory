96c2e53f530c1de308f92f9880b49686554b208a
Duana Saskia <duana@soundcloud.com>
2013-08-26 15:01:40 +0200

    Fix resource rating not clicking problem.
    Was due to turbolinks.js. The problem is described here http://asciicasts.com/episodes/390-turbolinks.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
+# solves the problem that jquery onload stuff doesn't load when clicking via a turbolin. Read more: http://asciicasts.com/episodes/390-turbolinks
+gem 'jquery-turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -1,4 +1,4 @@
-$(document).on('page:load', function() {
+$(function(){
 	$('.rating-button').click(function() {
 		$(this).closest('.resource-box').find('.rating-box').css("visibility", "visible");
 	});
@@ -8,19 +8,19 @@ $(document).on('page:load', function() {
     $('a.action.delete-comment').click(function(){
         return confirm('Are you sure you want to delete this comment?');
     });
-}); 
+});
 
 
-$(document).on('page:load', function() {
+$(function(){
     $('.myCheckbox').on('click', function(event) {
         var checkbox = $(event.currentTarget).find('.checkbox');
         checkbox.trigger('click');
     });
-    
+
     $('.myCheckbox .checkbox').on('change', function(event){
         var checkbox = $(event.target);
         checkbox.parent().toggleClass('checked');
     });
 });
 
-//'$(function(){}) is same as '$(document).ready(function() {}) 
+//'$(function(){}) is same as '$(document).ready(function() {})
