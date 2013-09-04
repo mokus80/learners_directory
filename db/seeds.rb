f5b3d0d94b527b852e57800640b55cb8afd4c819
Nicole <mokus80@gmail.com>
2013-09-04 17:27:07 +0200

    populate seed.rb file with tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tag.create([
+	{name: 'Rails'}, 
+	{name: 'Ruby'}, 
+	{name: 'Paid'}, 
+	{name: 'Free'},
+])
