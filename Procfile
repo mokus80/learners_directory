526b494eb4097f69fb2372e7b7634e4394781b7a
Nicole <mokus80@gmail.com>
2013-09-05 17:20:45 +0200

    added foreman gem, set local ENV keys

---
+++ Procfile
@@ -0,0 +1,1 @@
+dev: rails s
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-	provider :github, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
+		provider :github, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
 
 end
