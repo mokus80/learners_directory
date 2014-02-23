d034226d9fe5a8f4a653c3e08180ca6dba287be8
Nicole <mokus80@gmail.com>
2014-02-23 13:53:01 +0100

    save github login name instead of actual name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_or_create_by(:email => auth_hash[:info][:email], :name => auth_hash[:info][:name])
+    user = User.find_or_create_by(:email => auth_hash[:info][:email], :name => auth_hash[:info][:login])
     if user.nil?
     	redirect_to '/auth/:provider/callback'
     else
