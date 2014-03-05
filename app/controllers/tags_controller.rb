f9126c768b126a9f534d1f264b328fb0b13367a9
Nicole <mokus80@gmail.com>
2014-03-05 21:07:55 +0100

    admin can create and destroy resources

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,17 +1,35 @@
 class TagsController < ApplicationController
-	before_action :admin
-	
-	def index
-		@tags = Tag.all
-	end
+  before_action :admin
+  
+  def index
+    @tag = Tag.new
+    @tags = Tag.all
+  end
 
-	 def destroy
+  def create
+    @tag = Tag.new(tag_params)
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to tags_url, notice: "Tag was successfully created." }
+      else
+        format.html { redirect_to tags_url, notice: 'Please create a tag' }
+      end
+    end
+  end
+
+  def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
     respond_to do |format|
       format.html { redirect_to tags_path }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 
 end
