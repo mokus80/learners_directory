a3fecade2f9abc8dab5a61a7bdac20ddd0f0d2bc
Nicole <mokus80@gmail.com>
2014-03-05 19:27:37 +0100

    list tags and add link to delete them

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,16 @@ class TagsController < ApplicationController
 	before_action :admin
 	
 	def index
+		@tags = Tag.all
 	end
+
+	 def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    respond_to do |format|
+      format.html { redirect_to tags_path }
+      format.json { head :no_content }
+    end
+  end
 
 end
